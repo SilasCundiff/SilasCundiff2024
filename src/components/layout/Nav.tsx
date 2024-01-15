8985035bc219210150a2ad3e07bc7c9af7f04bb7
SilasCundiff <silinski23@gmail.com>
2024-01-15 15:14:58 -0500

    add working mobile nav

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,10 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubAlt, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 // import logo from assets
 import logo from "../../assets/Full-Logo.png";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Nav() {
   const scrollPosition = useScroll();
   const [isSticky, setIsSticky] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
     if (scrollPosition.y > window.innerHeight - 96) {
@@ -18,38 +20,55 @@ export default function Nav() {
   }, [scrollPosition]);
 
   return (
-    <nav
-      className={`flex w-full h-24 ${isSticky ? "fixed top-0" : "relative"}`}
-    >
-      <div className="flex justify-center items-center">
-        <a className="md:w-24 flex justify-center items-center" href="#home">
-          <img src={logo} alt="silas cundiff logo" height={64} width={64} />
-        </a>
+    <>
+      <div
+        className="fixed bottom-8 right-8 text-3xl md:hidden z-10 cursor-pointer w-12 h-12 flex justify-center items-center select-none"
+        onClick={() => {
+          setNavOpen(!navOpen);
+        }}
+      >
+        <FontAwesomeIcon icon={faBars} height={48} width={48} />
       </div>
-      <ul className="flex w-full bg-blue-300 gap-8">
-        <NavItem url="#skills">
-          <a>skill tree</a>
-        </NavItem>
-        <NavItem url="#projects">
-          <a>projects deck</a>
-        </NavItem>
-        <NavItem url="#about">
-          <a>lore</a>
-        </NavItem>
-        <NavItem url="#contact">/whisper</NavItem>
+      <nav
+        className={`translate-x-full transition-transform fixed right-0 h-svh flex md:flex-row flex-col w-40 md:w-full md:h-24 md:translate-x-0 select-none ${
+          isSticky ? "md:fixed top-0" : "md:relative"
+        }
+        ${navOpen ? "translate-x-0" : ""}
+        `}
+      >
+        <div className="flex justify-center items-center">
+          <a
+            className="md:w-24 h-24 flex justify-center items-center"
+            href="#home"
+          >
+            <img src={logo} alt="silas cundiff logo" height={64} width={64} />
+          </a>
+        </div>
+        <ul className="flex md:flex-row flex-col flex-1 items-center w-full mt-4 md:mt-0 bg-blue-300 gap-4 md:gap-8">
+          <NavItem url="#skills">
+            <a>skill tree</a>
+          </NavItem>
+          <NavItem url="#projects">
+            <a>projects deck</a>
+          </NavItem>
+          <NavItem url="#about">
+            <a>lore</a>
+          </NavItem>
+          <NavItem url="#contact">/whisper</NavItem>
 
-        <li className="my-auto text-white text-2xl">
-          <a target="_blank" href="https://github.com/SilasCundiff">
-            <FontAwesomeIcon icon={faGithubAlt} />
-          </a>
-        </li>
-        <li className="my-auto text-white text-2xl mr-8">
-          <a target="_blank" href="https://www.linkedin.com/in/silascundiff/">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-        </li>
-      </ul>
-    </nav>
+          <li className="md:my-auto my-2 text-white text-2xl">
+            <a target="_blank" href="https://github.com/SilasCundiff">
+              <FontAwesomeIcon icon={faGithubAlt} />
+            </a>
+          </li>
+          <li className="md:my-auto my-2 text-white text-2xl md:mr-8">
+            <a target="_blank" href="https://www.linkedin.com/in/silascundiff/">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
@@ -61,8 +80,8 @@ const NavItem = ({
   url: string;
 }) => {
   return (
-    <li className="my-auto first-of-type:ml-auto">
-      <a className="py-2 px-4 rounded-sm bg-white " href={url}>
+    <li className="md:my-auto my-4 md:first-of-type:ml-auto">
+      <a className="py-2 px-4 rounded-sm" href={url}>
         {children}
       </a>
     </li>
