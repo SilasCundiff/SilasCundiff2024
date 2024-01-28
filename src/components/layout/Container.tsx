7398fe06178c5b8729f4f6923e6abefc3a9b217e
SilasCundiff <silinski23@gmail.com>
2024-01-28 10:46:16 -0500

    clean dir

--- src/components/layout/Container.tsx
+++
@@ -1,3 +0,0 @@
-export default function Container({ children, gap = true }: { children: React.ReactNode; gap?: boolean }) {
-  return <div className={`container m-auto grid grid-cols-12 p-4 ${gap ? 'gap-6' : ''}`}>{children}</div>
-}
