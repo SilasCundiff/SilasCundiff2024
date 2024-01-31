f1158fb5b50baae4ea0f75d229319c5928f20f0d
SilasCundiff <silinski23@gmail.com>
2024-01-31 13:13:04 -0500

    testing react-spring

--- src/components/experiences/CardGameExperience/Card.tsx
+++ src/components/experiences/CardGameExperience/Card.tsx
@@ -6,139 +6,156 @@ import { gsap } from 'gsap'
 import { Text, useFont } from '@react-three/drei'
 import { useCardDropZoneContext } from '@/helpers/contexts/CardDropZoneContext'
 import { useCardDraggingContext } from '@/helpers/contexts/CardDraggingContext'
-import { a, useTransition, useSpring } from '@react-spring/three'
+import { a, useTransition, useSpring, animated } from '@react-spring/three'
 
 type CardProps = {
   cardId: string
   color: string
   index: number
-  position: Vector3
-  rotation: Euler
   cardWidth: number
   cardHeight: number
 }
 
-export default function Card({ cardId, index, color, position, rotation, cardWidth, cardHeight, ...props }: CardProps) {
+export default function Card({ cardId, index, color, cardWidth, cardHeight, ...props }: CardProps) {
   const { cardInDropZone, setCardInDropZone, cardDropZonePosition, cardDropZoneRotation } = useCardDropZoneContext()
   const { isCardBeingDragged, setIsCardBeingDragged } = useCardDraggingContext()
   const [isCardActive, setIsCardActive] = useState(false)
   const { size, viewport } = useThree()
   const aspect = useRef(size.width / viewport.width)
   const cardRef = useRef<any>(null)
-  const originalPosition = useRef<Vector3 | null>(position)
-  const originalRotation = useRef<Euler | null>(rotation)
-  const realTimePositionRef = useRef<Vector3 | null>(position)
-  const realTimeRotationRef = useRef<Euler | null>(rotation)
+  // const originalPosition = useRef<Vector3 | null>(position)
+  // const originalRotation = useRef<Euler | null>(rotation)
+  // const realTimePositionRef = useRef<Vector3 | null>(position)
+  // const realTimeRotationRef = useRef<Euler | null>(rotation)
 
-  useFrame((_state) => {
-    if (cardRef.current && realTimePositionRef.current) {
-      cardRef.current.position.x = realTimePositionRef.current.x
-      cardRef.current.position.y = realTimePositionRef.current.y
-      cardRef.current.position.z = realTimePositionRef.current.z
-    }
-    if (cardRef.current && realTimeRotationRef.current) {
-      cardRef.current.rotation.x = realTimeRotationRef.current.x
-      cardRef.current.rotation.y = realTimeRotationRef.current.y
-      cardRef.current.rotation.z = realTimeRotationRef.current.z
-    }
-  })
+  // const { cardPosition, cardRotation, cardScale } = useSpring({
+  //   from: {
+  //     cardPosition: [0, 0, 0],
+  //     cardRotation: [0, 0, 0],
+  //     cardScale: 0.5,
+  //   },
+  //   to: {
+  //     cardPosition: position,
+  //     cardRotation: rotation,
+  //     cardScale: 1,
+  //   },
+  //   config: {
+  //     mass: 1,
+  //     tension: 170,
+  //     friction: 26,
+  //   },
+  //   immediate: true,
+  // })
 
-  const checkOverlap = useCallback(() => {
-    const cardPos = realTimePositionRef.current
-    const dropZonePos = cardDropZonePosition
-    const distance = cardPos?.distanceTo(dropZonePos)
+  // useFrame((_state) => {
+  //   if (cardRef.current && realTimePositionRef.current) {
+  //     cardRef.current.position.x = realTimePositionRef.current.x
+  //     cardRef.current.position.y = realTimePositionRef.current.y
+  //     cardRef.current.position.z = realTimePositionRef.current.z
+  //   }
+  //   if (cardRef.current && realTimeRotationRef.current) {
+  //     cardRef.current.rotation.x = realTimeRotationRef.current.x
+  //     cardRef.current.rotation.y = realTimeRotationRef.current.y
+  //     cardRef.current.rotation.z = realTimeRotationRef.current.z
+  //   }
+  // })
 
-    if (!distance) return false
+  // const checkOverlap = useCallback(() => {
+  //   const cardPos = realTimePositionRef.current
+  //   const dropZonePos = cardDropZonePosition
+  //   const distance = cardPos?.distanceTo(dropZonePos)
 
-    return distance <= 2.75
-  }, [realTimePositionRef, cardDropZonePosition])
+  //   if (!distance) return false
 
-  const bind = useGesture(
-    {
-      onDrag: ({ event }) => {
-        event.stopPropagation()
-        setIsCardBeingDragged(true)
-        // @ts-ignore
-        realTimePositionRef.current = new Vector3(event.point.x, event.point.y, 2)
-        realTimeRotationRef.current = new Euler(0, 0, 0)
-      },
-      onDragEnd: ({ event }) => {
-        event.stopPropagation()
-        const zOffset = 0.1
-        setIsCardBeingDragged(false)
-        if (checkOverlap()) {
-          if (!isCardActive) {
-            setCardInDropZone(cardId)
-          }
-          gsap.to(realTimePositionRef.current, { ...cardDropZonePosition.clone().setZ(zOffset), duration: 0.2 })
-          gsap.to(realTimeRotationRef.current, { ...cardDropZoneRotation.clone(), duration: 0.2 })
-        } else if (!checkOverlap()) {
-          setCardInDropZone(cardInDropZone === cardId ? null : cardInDropZone)
-          gsap.to(realTimePositionRef.current, { ...originalPosition.current, duration: 0.2 })
-          gsap.to(realTimeRotationRef.current, { ...originalRotation.current, duration: 0.2 })
-        }
-      },
-      onHover: ({ active, event }) => {
-        event.stopPropagation()
-        if (isCardBeingDragged) return
-        if (active) {
-          gsap.to(cardRef.current.position, { z: 1.1, duration: 0.1 })
-          gsap.to(cardRef.current.scale, { x: 1.1, y: 1.1, duration: 0.1 })
-        } else {
-          gsap.to(cardRef.current.position, { z: 1, duration: 0.1 })
-          gsap.to(cardRef.current.scale, { x: 1, y: 1, duration: 0.1 })
-        }
-      },
-    },
-    {
-      drag: {},
-    },
-  )
+  //   return distance <= 2.75
+  // }, [realTimePositionRef, cardDropZonePosition])
 
-  // Swaps out the active card when a new card is dragged over the drop zone while it has a card already
-  useLayoutEffect(() => {
-    if (cardInDropZone === cardId) {
-      if (checkOverlap()) {
-        setIsCardActive(true)
-      }
-    } else {
-      setIsCardActive(false)
-      gsap.to(realTimePositionRef.current, { ...originalPosition.current, duration: 0.2 })
-      gsap.to(realTimeRotationRef.current, { ...originalRotation.current, duration: 0.2 })
-    }
-  }, [cardInDropZone, cardId, originalPosition, originalRotation, checkOverlap])
+  // const bind = useGesture(
+  //   {
+  //     onDrag: ({ event }) => {
+  //       event.stopPropagation()
+  //       setIsCardBeingDragged(true)
+  //       // @ts-ignore
+  //       realTimePositionRef.current = new Vector3(event.point.x, event.point.y, 2)
+  //       realTimeRotationRef.current = new Euler(0, 0, 0)
+  //     },
+  //     onDragEnd: ({ event }) => {
+  //       event.stopPropagation()
+  //       const zOffset = 0.1
+  //       setIsCardBeingDragged(false)
+  //       if (checkOverlap()) {
+  //         if (!isCardActive) {
+  //           setCardInDropZone(cardId)
+  //         }
+  //         gsap.to(realTimePositionRef.current, { ...cardDropZonePosition.clone().setZ(zOffset), duration: 0.2 })
+  //         gsap.to(realTimeRotationRef.current, { ...cardDropZoneRotation.clone(), duration: 0.2 })
+  //       } else if (!checkOverlap()) {
+  //         setCardInDropZone(cardInDropZone === cardId ? null : cardInDropZone)
+  //         gsap.to(realTimePositionRef.current, { ...originalPosition.current, duration: 0.2 })
+  //         gsap.to(realTimeRotationRef.current, { ...originalRotation.current, duration: 0.2 })
+  //       }
+  //     },
+  //     onHover: ({ active, event }) => {
+  //       event.stopPropagation()
+  //       if (isCardBeingDragged) return
+  //       if (active) {
+  //         gsap.to(cardRef.current.position, { z: 1.1, duration: 0.1 })
+  //         gsap.to(cardRef.current.scale, { x: 1.1, y: 1.1, duration: 0.1 })
+  //       } else {
+  //         gsap.to(cardRef.current.position, { z: 1, duration: 0.1 })
+  //         gsap.to(cardRef.current.scale, { x: 1, y: 1, duration: 0.1 })
+  //       }
+  //     },
+  //   },
+  //   {
+  //     drag: {},
+  //   },
+  // )
 
-  //update aspect ratio to fix weird card cursor offset misalignment
-  useEffect(() => {
-    aspect.current = size.width / viewport.width
-  }, [size, viewport])
+  // // Swaps out the active card when a new card is dragged over the drop zone while it has a card already
+  // useLayoutEffect(() => {
+  //   if (cardInDropZone === cardId) {
+  //     if (checkOverlap()) {
+  //       setIsCardActive(true)
+  //     }
+  //   } else {
+  //     setIsCardActive(false)
+  //     gsap.to(realTimePositionRef.current, { ...originalPosition.current, duration: 0.2 })
+  //     gsap.to(realTimeRotationRef.current, { ...originalRotation.current, duration: 0.2 })
+  //   }
+  // }, [cardInDropZone, cardId, originalPosition, originalRotation, checkOverlap])
 
-  // Animate card to it's hand position on mount
-  useLayoutEffect(() => {
-    const delay = 0.2 * index
-    // Define the offscreen position
-    const offScreenPosition = { x: -window.innerWidth, y: -window.innerHeight, z: position.z }
-    const offScreenRotation = { x: rotation.x, y: rotation.y, z: rotation.z }
+  // //update aspect ratio to fix weird card cursor offset misalignment
+  // useEffect(() => {
+  //   aspect.current = size.width / viewport.width
+  // }, [size, viewport])
 
-    // Animate the card from the offscreen position to its final position
-    gsap.fromTo(
-      realTimePositionRef.current,
-      { ...offScreenPosition },
-      { ...position, duration: 0.75, delay, ease: 'power1.out' },
-    )
-    gsap.fromTo(
-      realTimeRotationRef.current,
-      { ...offScreenRotation },
-      { ...rotation, duration: 0.75, delay, ease: 'power1.out' },
-    )
+  // // Animate card to it's hand position on mount
+  // useLayoutEffect(() => {
+  //   const delay = 0.2 * index
+  //   // Define the offscreen position
+  //   const offScreenPosition = { x: -window.innerWidth, y: -window.innerHeight, z: position.z }
+  //   const offScreenRotation = { x: rotation.x, y: rotation.y, z: rotation.z }
 
-    return () => {}
-  }, [index, position, rotation, realTimePositionRef, realTimeRotationRef])
-  console.log('Card rendered')
+  //   // Animate the card from the offscreen position to its final position
+  //   gsap.fromTo(
+  //     realTimePositionRef.current,
+  //     { ...offScreenPosition },
+  //     { ...position, duration: 0.75, delay, ease: 'power1.out' },
+  //   )
+  //   gsap.fromTo(
+  //     realTimeRotationRef.current,
+  //     { ...offScreenRotation },
+  //     { ...rotation, duration: 0.75, delay, ease: 'power1.out' },
+  //   )
+
+  //   return () => {}
+  // }, [index, position, rotation, realTimePositionRef, realTimeRotationRef])
+  // console.log('Card rendered')
 
   return (
     // @ts-ignore
-    <group position={realTimePositionRef.current} rotation={realTimeRotationRef.current} {...bind()} ref={cardRef}>
+    <>
       <mesh>
         <planeGeometry args={[cardWidth, cardHeight, 1]} />
         <meshBasicMaterial color={color} />
@@ -154,7 +171,7 @@ export default function Card({ cardId, index, color, position, rotation, cardWid
         {cardId}
         <meshBasicMaterial color={'#000'} />
       </Text>
-    </group>
+    </>
   )
 }
 
--- src/components/experiences/CardGameExperience/Hand.tsx
+++ src/components/experiences/CardGameExperience/Hand.tsx
@@ -2,9 +2,13 @@ import { Euler, Vector3 } from 'three'
 import Card from './Card'
 import { useDeckAndHandContext } from '@/helpers/contexts/DeckAndHandContext'
 import { CARD_HEIGHT, CARD_WIDTH } from '@/helpers/constants'
+import { animated, useSpringRef, useTransition } from '@react-spring/three'
+import { useEffect } from 'react'
+import { useBounds } from '@react-three/drei'
 
 export default function Hand() {
   const { hand } = useDeckAndHandContext()
+  const bounds = useBounds()
   const cardsInHandPositions = [
     new Vector3(-2, -2.4, 1.3),
     new Vector3(-1, -2.15, 1.2),
@@ -19,26 +23,67 @@ export default function Hand() {
     new Euler(0, 0, -0.15),
     new Euler(0, 0, -0.25),
   ]
+  // hand &&
+  //   hand.map(({ id, color }, index) => {
+  //     const cardPosition = cardsInHandPositions[index]
+  //     cardPosition.z = 0.01 * index + 0.01 // fix z-fighting
+  //     return (
+  //       <Card
+  //         key={id}
+  //         cardId={id}
+  //         index={index}
+  //         color={color}
+  //         position={cardPosition}
+  //         rotation={cardsInHandRotations[index]}
+  //         cardWidth={CARD_WIDTH}
+  //         cardHeight={CARD_HEIGHT}
+  //       />
+  //     )
+  //   })
+  const transitionRef = useSpringRef()
+  const transition = useTransition(hand, {
+    ref: transitionRef,
+    keys: hand.map(({ id }) => id),
+    from: { position: new Vector3(-window.innerWidth, -window.innerHeight, 0), rotation: new Euler(0, 0, 0) },
+    enter: (card) => {
+      const index = hand.indexOf(card)
+      console.log('index', index, card, cardsInHandPositions[index])
+      return { position: cardsInHandPositions[index], rotation: cardsInHandRotations[index] }
+    },
+    leave: { position: new Vector3(window.innerWidth, window.innerHeight, 0), rotation: new Euler(0, 0, 0) },
+    update: (card) => {
+      const index = hand.indexOf(card)
+      return { position: cardsInHandPositions[index], rotation: cardsInHandRotations[index] }
+    },
+    config: {
+      mass: 5,
+      friction: 120,
+      tension: 120,
+    },
+    trail: 100,
+  })
+  // console.log('hand', hand, transitionRef, transition)
+
+  useEffect(() => {
+    transitionRef.start()
+  }, [hand, transitionRef])
 
   return (
     <group>
-      {hand &&
-        hand.map(({ id, color }, index) => {
-          const cardPosition = cardsInHandPositions[index]
-          cardPosition.z = 0.01 * index + 0.01 // fix z-fighting
-          return (
+      {transition((props, card) => {
+        return (
+          <animated.group position={props.position} rotation={props.rotation}>
             <Card
-              key={id}
-              cardId={id}
-              index={index}
-              color={color}
-              position={cardPosition}
-              rotation={cardsInHandRotations[index]}
+              key={card.id}
+              cardId={card.id}
+              index={hand.indexOf(card)}
+              color={card.color}
               cardWidth={CARD_WIDTH}
               cardHeight={CARD_HEIGHT}
             />
-          )
-        })}
+          </animated.group>
+        )
+      })}
     </group>
   )
 }
