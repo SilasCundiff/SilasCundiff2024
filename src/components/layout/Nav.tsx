11f4fd5aafdb925f90755991d30b5e687246a1f6
SilasCundiff <silinski23@gmail.com>
2024-06-24 13:59:58 -0400

    finish styles for desktop nav refactor

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -6,6 +6,7 @@ import { GithubIcon, LinkedInIcon } from '../common/icons'
 
 export default function Nav() {
   const inventoryItems = [
+    { name: 'home', url: '#home' },
     { name: 'skills', url: '#skills' },
     { name: 'projects', url: '#projects' },
     { name: 'lore', url: '#about' },
@@ -20,27 +21,31 @@ export default function Nav() {
       if (inventoryItems[i]) {
         inventory.push(
           <NavItem key={i} url={inventoryItems[i].url} target={inventoryItems[i].target} slotNumber={i + 1}>
-            <div className='flex items-center justify-center mx-auto sm:mb-1 w-9/12'>
+            <div className='flex items-center justify-center mx-auto sm:mb-1 w-7/12'>
               {inventoryItems[i].icon ? inventoryItems[i].icon : null}
             </div>
             <div className='sm:block hidden'>{inventoryItems[i].name}</div>
           </NavItem>,
         )
       } else {
-        inventory.push(<li className=' text-slate-900 text-center inventory-item' key={i}></li>)
+        inventory.push(
+          <NavItem key={i} slotNumber={i + 1}>
+            <p>{''}</p>
+          </NavItem>,
+        )
       }
     }
     return inventory
   }
 
   return (
-    <div className='flex justify-center items-center fixed bottom-2 left-[50%] -translate-x-[50%] z-40 bg-transparent'>
+    <div className='flex justify-center items-center fixed bottom-4 left-[50%] -translate-x-[50%] z-40 bg-transparent'>
       <nav
         className={` 
              select-none  font-pressStart
-             container  bg-white inventory-border w-full p-0`}
+             max-w-[680px] inventory-border w-full p-0 py-.5`}
       >
-        <ul className='nav-inventory-grid gap-0.5'>{fillInventory()}</ul>
+        <ul className='nav-inventory-grid gap-1'>{fillInventory()}</ul>
       </nav>
     </div>
   )
@@ -53,18 +58,18 @@ const NavItem = ({
   slotNumber,
 }: {
   children: React.ReactNode
-  url: string
+  url?: string
   target?: string
   slotNumber: number
 }) => {
   return (
-    <li className=' text-slate-900 text-center inventory-item relative'>
-      <span className='absolute top-0.5 left-1.5 text-[.45em] text-slate-400'>{slotNumber}</span>
+    <li className=' text-slate-900 text-center inventory-item relative hover:bg-black/15 transition-colors'>
+      <span className='absolute top-1 left-1.5 text-[.45em] text-slate-400'>{slotNumber}</span>
       <a
         target={target}
-        className='text-center text-[.5em] flex flex-col justify-center items-center text-white w-full h-full sm:p-2 p-1 gap-1'
+        className='text-center flex flex-col justify-center items-center text-white w-full h-full p-1.5 gap-1'
         href={url}
-        style={{ fontSize: 'clamp(6px, .5vw, 20px)' }}
+        style={{ fontSize: 'clamp(6px, .4vw, 8px)' }}
       >
         {children}
       </a>
